7ace6229b225ec738c555a04a545cdaa2904b166
Joseph Chung <chung@Josephs-MBP.lan>
2021-02-18 23:46:04 -0600

    apis

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
 
     render(){
         return (
-            <div>Hello! </div>
+            <div>Hello!    </div>
         );
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(<App/>, document.querySelector("#root"));
