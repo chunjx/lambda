91b1109b40bb57ed45499e57ecea05b77606210b
Joseph Chung <chung@Josephs-MBP.lan>
2021-02-18 22:23:28 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import lambda from './apis/lambda';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+    componentDidMount = async () => {
+        const response = await lambda.get('', {
+            params: {
+              TableName: 'MyTable'
+            }
+        });
+    };
+
+    render(){
+        return (
+            <div>Hello! </div>
+        );
+    };
+};
 
 export default App;
+
---
+++ src/apis/lambda.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+   baseURL: "https://kcb6hw7ks2.execute-api.us-east-2.amazonaws.com/default/myfunction" 
+});
